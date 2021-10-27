f5bfd3dd7e678b95443d4be09a5cbb19848d066e
Raphael Graham <grahamraphael88@gmail.com>
2021-10-27 00:19:45 +0000

    Last one

---
+++ mid-sem/screens/cart.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View,Text,TextInput,Image,TouchableOpacity} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+export default function Cart(){
+    return <View>
+        
+    </View>
+}
---
+++ mid-sem/screens/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View,Text,Image,TextInput,TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+
+
+export default function Login(){
+    return <View>
+         
+    </View>
+}
+   
