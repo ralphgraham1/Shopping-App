d3a022462249c985f258cc7a875f0d61f8fec22a
Raphael Graham <grahamraphael88@gmail.com>
2021-11-17 21:10:53 +0000

    almost there

--- mid-sem/Components/data/products.js
+++ mid-sem/Components/data/products.js
@@ -6,7 +6,8 @@ export default [
         image: require("../../assets/Canon6d.jpg"),
         price: "GHC9000.00",
         ratings: 1345,
-        avgRating: 4.78
+        avgRating: 4.78,
+        description:'Step up into the full-frame game with the EOS 6D Mark II DSLR Camera from Canon Making high-performance full-frame imaging more accessible, the 6D Mark II makes numerous advancements over its predecessor to improve upon every aspect of image capture and creation. The main update comes in the form of a 26.2MP CMOS sensor and the use of the DIGIC 7 Image Processor to deliver sharper, more vivid imagery in a variety of shooting situations. This setup offers an improved native sensitivity range of ISO 100-40000, which can then be expanded to ISO 50-102400, as well as speedy shooting rates of up to 6.5 fps. '
         
     },
     {
@@ -15,7 +16,8 @@ export default [
         image: require('../../assets/24-70mm.jpeg'),
         price: " GHC14,753.85",
          ratings: 9876,
-         avgRating: 4.7
+         avgRating: 4.7,
+         description:'Poised to be the new workhorse zoom, the Canon RF 24-70mm f/2.8L IS USM is a versatile wide-angle to portrait-length lens characterized by its bright, advanced, and intuitive design. The constant f/2.8 maximum aperture suits working in difficult lighting conditions and also enables greater control over depth of field. The optical design incorporates a series of aspherical and Ultra-Low Dispersion elements, which greatly reduce a variety of aberrations throughout the zoom range in order to produce sharp and clear imagery. An Air Sphere Coating has also been applied to suppress flare and ghosting when working in strong lighting conditions.'
      },
     {
        id: "3",
@@ -23,7 +25,8 @@ export default [
         image: require('../../assets/50mm.png'),
         price: "GHC1,223.85",
         ratings: 2445,
-         avgRating: 4.7
+         avgRating: 4.7,
+         description:'Compact, lightweight, and close-focusing, the Canon RF 50mm f/1.8 STM is a sleek normal-length prime offering a classic, versatile field of view and an especially compact design. An ideal lens for every day, walkaround shooting, this 50mm f/1.8 is characterized by its bright design for available light shooting and apt depth of field control. The optical design incorporates one aspherical element, which reduces spherical aberrations and distortion, and also sports a Super Spectra coating to minimize flare and ghosting for improved contrast and color accuracy. Also, an STM motor affords impressively quiet and smooth autofocus performance and full-time manual focus override. Additionally, a configurable Control Ring lets you adjust exposure settings, including ISO, aperture, and exposure compensation, from the lens itself.',
     },
      {
         id: "4",
@@ -31,7 +34,8 @@ export default [
         image: require('../../assets/Canont7.jpg'),
         price: "GHC2,761.35",
         ratings: 6431,
-       avgRating: 4.7
+       avgRating: 4.7,
+       description:'Compact and capable, the Canon EOS Rebel T7 is a sleek entry-level DSLR featuring versatile imaging capabilities and a helpful feature-set. Incorporating a 24.1MP APS-C CMOS sensor and DIGIC 4+ image processor, the T7 produces high-resolution stills with notable clarity, reduced noise, and a flexible native sensitivity range from ISO 100-6400 for working in a variety of lighting conditions. The sensor and processor also afford shooting continuously at up to 3 fps for photographing moving subjects, as well as Full HD 1080/30p video recording. Benefitting shooting with the optical viewfinder, a 9-point AF system incorporates a single center cross-type AF point for increased accuracy and focusing speed, and a contrast-detection focusing system offers refined autofocus precision when recording video or photographing in live view. The rear 3.0" 920k-dot LCD offers a bright, clear image for playback and review, and built-in Wi-Fi with NFC also offers the ability to wirelessly share imagery from your camera to a linked mobile device for instant online sharing.',
      },
      {
         id: "5",
@@ -40,7 +44,9 @@ export default [
         price: "GHC21,512.70",
          oldPrice:"$199.99",
          ratings: 1345,
-        avgRating: 4.7
+        avgRating: 4.7,
+        description:'Moving to an even higher resolution realm, the Sony a7R IVA continues the noteworthy series of highly capable full-frame cameras, characterized by impressive stills and video performance and versatility. This fourth edition of the a7R sees the inclusion of an updated 61MP Exmor R BSI CMOS sensor and enhanced BIONZ X image processor, which afford impressive imaging results with marked smoothness, a 15-stop dynamic range, and, of course, speed. Up to 10 fps shooting is possible along with the ability to record UHD 4K30 video, both with support for Real-time Eye AF and Tracking AF technologies. The revised sensor design uses a Fast Hybrid AF System, with 567 phase-detection autofocus points, for notably fast and accurate focusing performance. Despite being such a high-resolution sensor, it is also distinguished by its flexible native sensitivity range from ISO 100-32000 for working in a variety of lighting conditions. Additionally, benefitting both stills and video operation, the a7R IVA retains the 5-axis SteadyShot INSIDE sensor-shift image stabilization, which is effective to minimize the appearance of camera shake by up to 5.5 stops.'
+        
     },
      {
         id: "6",
@@ -48,7 +54,8 @@ export default [
          image: require('../../assets/eosr3.jpg'),
         price: "GHC36,893.85",
         ratings: 2425,
-        avgRating: 4.7
+        avgRating: 4.7,
+        description:'Built for speed, versatility, and reliability, the Canon EOS R3 combines the technology from their mirrorless EOS R system with the robustness and performance you\'d expect from a flagship DSLR. Revolving around a new full-frame stacked sensor, updated AF performance, and refined body design, the EOS R3 is first 3-Series camera since the film era and plants itself as a fast-shooting, flexible, and contemporary mirrorless camera.',
     },
      {
         id: "7",
@@ -56,7 +63,8 @@ export default [
         image: require('../../assets/eosrebel.jpg'),
          price: "GHC 4298.85",
         ratings: 7543,
-        avgRating: 4.7
+        avgRating: 4.7,
+        description:'Defined by its notably sleek form factor, the black Canon EOS Rebel SL3 is a versatile DSLR offering an ideal mix of stills and video capabilities. Featuring a 24.1MP APS-C-format CMOS sensor and DIGIC 8 image processor, high-resolution stills and UHD 4K video recording are possible, along with a wide ISO 100-25600 sensitivity range to suit working in a variety of lighting conditions. In-camera 4K time-lapse movie creation is also possible for creatively depicting subjects over time. Benefitting the imaging attributes, the sensor\'s design also facilitates Dual Pixel CMOS AF, which uses phase-detection autofocus for quick, accurate, and smooth focusing performance when recording movies or shooting stills in live view. When shooting stills using the optical viewfinder, a 9-point phase-detection system is used for quick and accurate focusing in a variety of shooting scenarios.'
      },
     {
          id: "8",
@@ -64,7 +72,8 @@ export default [
          image:require('../../assets/eosrp.jpg'),
         price: "GHC6,143.85",
         ratings: 6544,
-        avgRating: 4.7
+        avgRating: 4.7,
+        description:'Impressively small despite having a large full-frame sensor, the Canon EOS RP offers flexible imaging capabilities along with a portable form factor. Featuring a high-resolution 26.2MP full-frame CMOS sensor along with a DIGIC 8 image processor, both stills and UHD 4K video can be recorded using a broad sensitivity range, from ISO 100-40000, to suit working in a variety of lighting conditions. Continuous shooting is also supported at up to 5 fps for photographing moving subjects. The sensor also facilitates an advanced Dual Pixel CMOS AF system, with 4779 selectable on-sensor phase-detection points for quickly and accurately acquiring focus during stills and video operation.'
     },
     {
         id: "9",
@@ -72,7 +81,8 @@ export default [
         image: require('../../assets/Nikonmirror.jpg'),
         price: "GHC35,337.90 ",
         ratings: 1564,
-        avgRating: 4.7
+        avgRating: 4.7,
+        description:'Delivering speed, image quality, and connectivity, the Nikon Z 9 is a professional-grade camera characterized by its newly developed sensor, advanced AF performance, and high-resolution 8K video recording. Their first mirrorless flagship camera, the Z 9 stands out as a camera that appeals to photographers and filmmakers working across a wide variety of applications.'
     },
      {
         id: "10",
@@ -80,7 +90,9 @@ export default [
         image: require('../../assets/r5.jpg'),
         price: "GHC23,978.85 ",
         ratings: 1234,
-        avgRating: 4.7
+        avgRating: 4.7,
+        description:'   '  
+
     },
     {
         id: "11",
